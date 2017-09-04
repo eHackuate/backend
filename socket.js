968b01de8a194180e75ca33992fe0ee185f260a8
erfanio <dev@erfan.io>
2017-09-04 23:49:27 +1000

    i send a list to front end also a room for frontend

--- socket.js
+++ socket.js
@@ -1,10 +1,52 @@
 const accountSid = process.env.ACCOUNTSID
 const token = process.env.TOKEN
-
-const twilio = new require('twilio')(accountSid, token)
-
 const fromNumber = process.env.FROM
 const toNumber = process.env.TO
+const twilio = new require('twilio')(accountSid, token)
+
+// TODO; replace redacted with actuall numbers
+const people = [
+  {
+    id: 0,
+    name: 'Erfan ilovemycoffee',
+    number: 'redacted',
+    role: 'Software Developer',
+    status: 'okay',
+    chain: []
+  },
+  {
+    id: 1,
+    name: 'Josh O\'Hackeroo',
+    number: 'redacted',
+    role: 'Software Developer',
+    status: 'idk',
+    chain: []
+  },
+  {
+    id: 2,
+    name: 'Eric McCode',
+    number: 'redacted',
+    role: 'Software Developer',
+    status: 'idk',
+    chain: []
+  },
+  {
+    id: 3,
+    name: 'Ramzi Danger',
+    number: 'redacted',
+    role: 'Software Developer',
+    status: 'bad',
+    chain: []
+  },
+  {
+    id: 4,
+    name: 'Em McBusiness',
+    number: 'redacted',
+    role: 'Product Manager',
+    status: 'okay',
+    chain: []
+  }
+];
 
 exports.register = (server, options, next) => {
   const io = require('socket.io')(server.listener);
@@ -12,20 +54,23 @@ exports.register = (server, options, next) => {
   io.on('connection', (socket) => {
     console.log(`${socket.id} connected!`);
 
-    socket.on('calendar', () => {
-      console.log(`${socket.id} calendar`);
-
-      socket.emit('calendar', { from: 1200, to: 1400, name: 'Meeting with client', location: 'Building 2, Meeting room 5' })
-    });
-
     socket.on('disconnection', () => {
       console.log(`${socket.id} disconnected`);
     });
+
+    // frontend has a special signal to join the room
+    socket.on('imfrontend', () => {
+      console.log(`${socket.id} is a front end!`);
+
+      socket.join('frontend');
+      socket.to('frontend').emit('update', people)
+    })
 
     socket.on('incident', () => {
       console.log(`${socket.id} incident received!`)
 
       // Send message
+      // /* <- this means don't send sms (comment the comment out to send sms... so meta)
       let promise = twilio.messages.create({
         from: fromNumber,
         to: toNumber,
@@ -37,8 +82,10 @@ exports.register = (server, options, next) => {
       promise.catch((err) => {
         console.error(err)
       })
+      /* I use this to not send sms when doing shit */
 
-      socket.emit('sms', { value: 'SMS', people: 'Test person' })
+      people.forEach((person) => person.chain.push(`New incident detected! Things are happening! Are you okay? (Y/N)`));
+      socket.to('frontend').emit('update', people)
     });
   });
 
