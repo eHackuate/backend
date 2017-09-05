05c5fad84fff8372948fc362144b104fddeaf0be
erfanio <dev@erfan.io>
2017-09-05 20:35:37 +1000

    twilio likes null better than json expects valid twiml xml response otherwise

--- sms.js
+++ sms.js
@@ -3,5 +3,5 @@ const { receiveReply } = require('./socket.js');
 module.exports = (request, reply) => {
   console.log('Received new SMS', JSON.stringify(request.payload, null, 2));
   receiveReply(request.payload);
-  reply({ status: 'success' });
+  reply(null);
 };
